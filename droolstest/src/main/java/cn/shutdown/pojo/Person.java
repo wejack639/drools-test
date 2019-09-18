8b03991ece7c2826dc3128c4d37580c68f20e8a3
jiangwujie <Jwj88888888>
2019-09-18 23:14:46 +0800

    Fact对象引入及对对象属性规则判断及操作

---
+++ droolstest/src/main/java/cn/shutdown/pojo/Person.java
@@ -0,0 +1,9 @@
+package cn.shutdown.pojo;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private String name;
+    private String age;
+}
--- droolstest/src/main/java/cn/shutdown/rulesHello/RulesHello.java
+++ droolstest/src/main/java/cn/shutdown/rulesHello/RulesHello.java
@@ -1,5 +1,6 @@
 package cn.shutdown.rulesHello;
 
+import cn.shutdown.pojo.Person;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
@@ -10,8 +11,13 @@ public class RulesHello {
         KieContainer kc = kss.getKieClasspathContainer();
         //是从kmodule.xml中取的 session
         KieSession ks = kc.newKieSession("testhelloworld");
+        Person p = new Person();
+        p.setName("小明");
+        p.setAge("18");
+        ks.insert(p);
         int count = ks.fireAllRules();
         System.out.println("共执行了" + count + "条规则");
+        System.out.println(p);
         ks.dispose();
     }
 }
